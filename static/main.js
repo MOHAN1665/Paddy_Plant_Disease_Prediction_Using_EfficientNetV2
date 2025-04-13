ef7fdfe5d49bd28a876726c0fa1ca808d2992a1f
Mohan Kumar <114055328+MOHAN1665@users.noreply.github.com>
2025-04-13 10:36:36 +0530

    Update main.js

--- static/main.js
+++ static/main.js
@@ -347,3 +347,13 @@ $(document).ready(function () {
     );
   });
 });
+
+// Smooth scrolling for all anchor links
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
